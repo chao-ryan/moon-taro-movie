fe963231a87375dfdcefe73cc322131681103229
dengchao <dengchao@intasect.com.cn>
2022-05-15 17:57:21 +0900

    update setting files

--- config/dev.js
+++ config/dev.js
@@ -1,9 +1,17 @@
+/* eslint-disable import/no-commonjs */
+
+// 根据需要修改小程序id
+const appId = "wx4f1abf7f92833937";
+
 module.exports = {
   env: {
     NODE_ENV: '"development"'
   },
   defineConstants: {
+    ENVIRONMENT: JSON.stringify("development"),
+    APP_ID: JSON.stringify(appId),
+    BASE_URL: JSON.stringify("http://192.168.6.145:3040") // 真正的api
   },
   mini: {},
   h5: {}
-}
+};
---
+++ config/prepub.js
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-commonjs */
+// 根据需要修改小程序id
+const appId = "wx51036ec567f75dad";
+
+module.exports = {
+  env: {
+    NODE_ENV: '"production"',
+  },
+  defineConstants: {
+    ENVIRONMENT: JSON.stringify("prepub"),
+    APP_ID: JSON.stringify(appId),
+    BASE_URL: JSON.stringify("http://192.168.6.145:3040"), // 真正的api
+  },
+  mini: {},
+  h5: {},
+};
